6cddc16a7ea897e59f540f744662901c19b25c83
Sujoy09 <mlflarkyy09@gmail.com>
2021-08-01 22:56:59 +0530

    REFACTORED:Test Case for State CodeCSV Analyser.

--- test/java/censusanalyser/CensusAnalyserTest.java
+++ test/java/censusanalyser/CensusAnalyserTest.java
@@ -6,8 +6,9 @@ import org.junit.rules.ExpectedException;
 
 public class CensusAnalyserTest {
 
-    private static final String INDIA_CENSUS_CSV_FILE_PATH = "C:\\Users\\roysu\\eclipse-workspace\\CensusAnalyser\\src\\test\\resources";
-    private static final String WRONG_CSV_FILE_PATH = "C:\\Users\\roysu\\eclipse-workspace\\CensusAnalyser\\src\\test\\resources";
+    private static final String INDIA_CENSUS_CSV_FILE_PATH = "C:\\Users\\roysu\\eclipse-workspace\\CensusAnalyser\\src\\test\\resources\\IndiaStateCensusData.csv";
+    private static final String WRONG_CSV_FILE_PATH = "C:\\Users\\roysu\\eclipse-workspace\\CensusAnalyser\\src\\main\\resources\\IndiaStateCensusData.csv";
+    private static final String INDIA_STATE_CSV_FILE_PATH = "C:\\Users\\roysu\\eclipse-workspace\\CensusAnalyser\\src\\test\\resources\\IndiaStateCode.csv";
 
     @Test
     public void givenIndianCensusCSVFileReturnsCorrectRecords() {
@@ -28,5 +29,14 @@ public class CensusAnalyserTest {
         } catch (CensusAnalyserException e) {
             Assert.assertEquals(CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM,e.type);
         }
+    }
+
+    @Test
+    public void givenIndiaStateCSV_ShouldReturnExactCount() throws CensusAnalyserException {
+        try {
+            CensusAnalyser censusAnalyser = new CensusAnalyser();
+            int numOfStateCode = censusAnalyser.loadIndianStateCode(INDIA_STATE_CSV_FILE_PATH);
+            Assert.assertEquals(37, numOfStateCode);
+        } catch (CensusAnalyserException e) {}
     }
 }
