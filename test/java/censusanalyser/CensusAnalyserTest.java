faf988c4734040b484917acda86cf9d79877fbf0
Sujoy09 <79314483+Sujoy09@users.noreply.github.com>
2021-08-01 20:01:00 +0530

    Update CensusAnalyserTest.java

--- test/java/censusanalyser/CensusAnalyserTest.java
+++ test/java/censusanalyser/CensusAnalyserTest.java
@@ -6,8 +6,8 @@ import org.junit.rules.ExpectedException;
 
 public class CensusAnalyserTest {
 
-    private static final String INDIA_CENSUS_CSV_FILE_PATH = "./src/test/resources/IndiaStateCensusData.csv";
-    private static final String WRONG_CSV_FILE_PATH = "./src/main/resources/IndiaStateCensusData.csv";
+    private static final String INDIA_CENSUS_CSV_FILE_PATH = "C:\Users\roysu\eclipse-workspace\CensusAnalyser\src\test\resources";
+    private static final String WRONG_CSV_FILE_PATH = "C:\Users\roysu\eclipse-workspace\CensusAnalyser\src\test\resources";
 
     @Test
     public void givenIndianCensusCSVFileReturnsCorrectRecords() {
